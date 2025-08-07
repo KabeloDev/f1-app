3355735062f1d2aea518ecfa1d9a042564208c0c
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-08-07 11:49:37 +0200

    Authentication > Updated firebase integration, created auth screen, sign out button in profile screen

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,15 +1,10 @@
-// Import the functions you need from the SDKs you need
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-import { getReactNativePersistence, initializeAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.EXPO_API_KEY,
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
   authDomain: "f1-app-3f81d.firebaseapp.com",
   projectId: "f1-app-3f81d",
   storageBucket: "f1-app-3f81d.firebasestorage.app",
@@ -20,7 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-});
+export const auth = initializeAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { auth } from "@/FirebaseConfig";
+import { router } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function ProfileScreen () {
+
+    const handleSignOut = () => {
+      auth.signOut;
+      router.replace('/auth/auth');
+    }
+
     return <View style={styles.body}>
         <Text>Profile screen</Text>
+        <TouchableOpacity onPress={() => handleSignOut()}>
+          <Text>Sign Out</Text>
+        </TouchableOpacity>
     </View>
 }
 
