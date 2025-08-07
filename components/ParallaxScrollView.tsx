200ce7335d8bfa8527268eefb57d68373e79059a
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-08-07 15:56:12 +0200

    Authentication > Styling

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
   header: {
     height: HEADER_HEIGHT,
     overflow: 'hidden',
-    borderRadius: 50
+    borderBottomLeftRadius: 50
   },
   content: {
     flex: 1,
